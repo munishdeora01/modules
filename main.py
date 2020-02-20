ff1bbc66f1b796ca6f3cb2c58b9fc69924831be7
munishdeora01 <61263807+munishdeora01@users.noreply.github.com>
2020-02-20 11:27:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import utility
+import shopping.moreshopping.shopping_cart
+
+
+print(utility.multiply(2,5))
+
+print(shopping.moreshopping.shopping_cart.buy('apple'))
---
+++ shopping/more_shopping/shopping_cart.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+def buy(item):
+    cart=[]
+    cart.append(item)
+    return cart
